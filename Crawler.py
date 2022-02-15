4eea0b2588926681e9ec59d0a247a04b4506acd9
LeeIkGyu <99635869+LeeIkGyu@users.noreply.github.com>
2022-02-15 21:04:03 +0900

    Add files via upload

---
+++ Crawler.py
@@ -0,0 +1,26 @@
+'''
+검색에 사용한 검색 엔진
+Torch, Ahmia, Onion Index Search Engine
+'''
+
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+import Key_word
+
+tor_proxy = "127.0.0.1:9150" # Tor 브라우저의 프록시 서버
+
+os.popen(r"C:\Users\SCHCsRC\Desktop\Tor Browser\Browser\firefox.exe") # Tor 브라우저 실행
+time.sleep(5)
+
+chrome_options = Options()
+chrome_options.add_argument('--ignore-certificate-errors') # SSL(안전하지 않음) 스킵
+chrome_options.add_argument('--disable-extensions') # 확장 프로그램 사용안함
+chrome_options.add_argument("--incognito") # 시크릿 모드로 실행
+chrome_options.add_argument('--user-data=C:\\Users\\SCHCsRC\\AppData\\Local\\Google\\Chrome\\User Data\\Default') # chrome 프로필을 사용
+chrome_options.add_argument('--proxy-server=socks5://%s' % tor_proxy) # 프록시
+driver = webdriver.Chrome(executable_path='chromedriver.exe', options=chrome_options)
+
+for key in Key_word.search_engine:
+    
---
+++ Key_word.py
@@ -0,0 +1,32 @@
+search_engine = {
+            "Torch" : "http://torchdeedp3i2jigzjdmfpn5ttjhthh5wbmda2rr3jvqjg5p77c54dqd.onion/search?query=",
+            "Ahmia" : "https://ahmia.fi/search/?q=",
+            "Onion Index Search Engine" : "http://oniondxjxs2mzjkbz7ldlflenh6huksestjsisc3usxht3wqgk6a62yd.onion/search?query=",
+            }
+
+# 영어
+S_edrug = ["drug", "dope", "narcotic", "opiate", "opium", "codeine", "cocaine", "opiates", "analgesic", "morphine",\
+            "opioids", "heroin", "narco", "amphetamine", "benzodiazepine", "cannabis", "hydrocodone", "muscle relaxant",\
+            "marijuana", "sedative", "psychostimulant", "hallucinogen", "narcotising", "methadone", "druggist", "paregoric"]
+S_eporno = ["Child Porn", "child sexual abuse", "Child abuse material", "Child exploitation material", "CSAM", "child sexploitation", "SexualExploitation", "CP"]
+S_ehack = ["hack", "hacker", "ransomeware", "malware", "spyware", "adware", "stuxnet", "trojan horse", "computer worm"\
+            "rootkit", "computer virus", "backdoor virus", "DDOS", "backdoor malware", "DDOS virus", "DDOS malware",\
+            "Malvertising"]
+S_epersonal = ["South Korea Personal Information", "South Korea Personal Information extrusion", "South Korea personal information theft",\
+            "South Korea personal information leak", "South Korea personal information hack", "South Korea data theft", "South Korea information leakage",\
+            "South Korea data leakage", "South Korea Private data", "South Korea Private data extrusion", "South Korea private data theft",\
+            "South Korea private data leak", "South Korea Carding", "South Korea gambler data", "South Korea Details Database", "South Korea DB"]
+
+# 한국어
+S_kdrug = ["마약", "떨", "작대기", "신경안전제", "근육이완제", "아편", "코카인", "헤로인", "대마초", "대마",\
+            "정신 자극제", "벤조디아제핀", "하이드로콘", "파레고리크", "메타돈", "환각제", "진정제", "암페타민", "오피오이드",\
+            "모르핀", "진통제", "코데인", "빙두", "캔디", "코끼리주사", "크랭크", "크리스탈", "얼음", "풀"] # 안전거래
+S_kporn = ["몰카", "몰카 야동", "아동 포르노", "리벤지 포르노", "골뱅이", "여고생", "여중생", "로리타", "여고생 야동", "여중생 야동", "어린아이"\
+            "국산 여중생", "국산 여고생", "여중생 따먹", "여고생 따먹", "여고생 포르노", "여중생 포르노", "고등어 야동", "고등어 포르노"]
+S_khack = ["핵", "해킹", "랜섬웨어", "악성코드", "악성프로그램", "멀웨어", "스파이웨어", "애드웨어", "스턱스넷", "트로이 목마", "컴퓨터 바이러스",\
+            "루트킷", "백도어 바이러스", "디도스 악성코드", "디도스 바이러스", "백도어 악성코드", "멀버타이징"]
+S_kpersonal = ["최신 디비 판매", "대출 디비 판매", "콜 디비 판매", "웹 디비 판매", "카지노 디비 판매", "보험 디비 판매", "주식 디비 판매",\
+            "스포츠토토 디비 판매", "디비 판매", "블랙 빅데이터 판매", "원시데이터 판매", "부결 디비 판매", "최신 DB 판매", "대출 DB 판매",\
+            "콜 DB 판매", "웹 DB 판매", "카지노 DB 판매", "보험 DB 판매", "주식 DB 판매", "스포츠토토 DB 판매", "DB 판매", "부결 DB 판매",\
+            "실시간 디비 판매", "실시간 DB 판매", "전날 디비 판매", "전날 DB 판매", "문자 DB 판매", "문자 디비 판매", "내구제 디비 판매",\
+            "내구제 DB 판매", "개인 정보 판매", "개인 디비 판매", "개인 DB 판매"]
