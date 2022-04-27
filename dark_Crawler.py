844fa3345bf319b6ae92e55f440930a653a1efc0
LeeIkGyu <leeik0276@gmail.com>
2022-04-27 18:44:34 +0900

    Add to Code

---
+++ dark_Crawler.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+class basics_parser():
+    def __init__(self, ):
+        pass
---
+++ mongodb.py
@@ -0,0 +1,2 @@
+from pymongo import MongoClient
+import traceback # 오류 정보 스택에 보이게 하기 위해 
